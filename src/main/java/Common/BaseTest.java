3af2c1ac997ba6c880bf6ad7372b2667c7a778e5
SGore <SGore@SILICUS339.silicus.com>
2017-12-27 12:23:02 +0530

    updated 27-12-2017 2

--- src/main/java/Common/BaseTest.java
+++ src/main/java/Common/BaseTest.java
@@ -1,23 +1,30 @@
 package Common;
 
+
+import java.net.MalformedURLException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+
 public class BaseTest {
 	protected WebDriver driver;
-	
+
 	public WebDriver getDriver() {
 		return driver;
 	}
 	
 	
 	@BeforeMethod
-	public void setDriver() {
+	public void setDriver() throws MalformedURLException {
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")  + "//browserproperty//chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		
+	
+	
+	
 		driver.get("http://opensource.demo.orangehrmlive.com/");
 	}
 	
